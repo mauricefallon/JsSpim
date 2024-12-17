f25ed971d371d140bf44e07dc28ab9cc6c39d1ff
Maurice Fallon <mauricefallon@gmail.com>
2024-12-17 16:41:36 +0000

    x

--- Tests/test.s
+++ Tests/test.s
@@ -1,6 +1,11 @@
 .text
 main:
-li $t0, 0           # mfallon
+# mfallon
+li $a0, 4
+jal sumall
+jr $ra
+
+#
 li $t1, 4
 loop: beq $t0, $t1, exit
 addi $t0, $t0, 1
@@ -50,6 +55,23 @@ bne $t2, $t4, loop
 # End program
 li $v0, 10
 syscall
+
+
+sumall:
+addi $sp, $sp, -4
+sw $s0, 0($sp)
+li $t0, 0                      #0
+move $t1, $a0
+move $s0, $zero                #added0
+loop: beq $t0, $t1, exit       #2
+add $s0, $t0, $s0              #added1
+addi $t0, $t0, 1               #3
+j loop                         #4
+exit:                          #5
+move $v0, $s0
+lw $s0, 0($sp)
+addi $sp, $sp, 4
+jr $ra
 
 fib:
 # Compute and return fibonacci number
