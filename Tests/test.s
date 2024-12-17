49c063efcbda064845a7901b2bccfcf084c36fdc
Maurice Fallon <mauricefallon@gmail.com>
2024-12-17 15:31:18 +0000

    x

--- Tests/test.s
+++ Tests/test.s
@@ -1,6 +1,8 @@
-li $t0, 0
-li $t1, 4
-loop: beq $t0, $t1, exit
-addi $t0, $t0, 1
-j loop
-exit:
+main:
+
+        li $t0, 0
+        li $t1, 4
+        loop: beq $t0, $t1, exit
+        addi $t0, $t0, 1
+        j loop
+        exit:
