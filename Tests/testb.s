c74b17fac3b672e4d2d8fb6d5db8fbc4122301a2
Maurice Fallon <mauricefallon@gmail.com>
2024-12-17 17:38:26 +0000

    Your Message

--- Tests/testb.s
+++ Tests/testb.s
@@ -1,4 +1,4 @@
-# 
+# 2022 exam
 .text
 main:
 li $a0, 6
@@ -9,17 +9,24 @@ syscall
 SumAll:
 addi $sp, $sp, -4
 sw $s0, 0($sp)
+
 # li - load immediate - sets $t0 to be 0
+# $t0 will be the for loop counter 0->6
 li $t0, 0                  #0 sets $t0 to be 0
+
+# test up the maximum value of t1=6
 move $t1, $a0
+# test the current value to be 0
 move $s0, $zero            #a0 puts zero into $s0
+
+# loop from t0 [0] to t1 [6] and then jump to exit
 loop: beq $t0, $t1, exit   #2
-add $s0, $t0, $s0          #a1
-addi $t0, $t0, 1           #3
-j loop                     #4
+add $s0, $t0, $s0          #a1 add t0 to s0
+addi $t0, $t0, 1           #3 i++
+j loop                     #4 end loop
 exit:                      #5
-move $v0, $s0
-lw $s0, 0($sp)
+move $v0, $s0         # move s0 to v0
+lw $s0, 0($sp)        # put the result in the stack
 addi $sp, $sp, 4
 jr $ra
 
---
+++ Tests/testc.s
@@ -0,0 +1,32 @@
+# the data to be used by the program
+# arr is an array, len is the length of the array
+.data
+arr: .word  2, 4, 1, 8, 10
+len: .word  5
+
+# the main program
+.text
+main:
+   li
+   $t0, 0
+   li
+   $t1, 1
+   lw $t7, len
+
+check:
+   bge $t0, $t7, exit
+   sll $t6, $t0, 2
+   lw  $t2, arr($t6)
+   # arr() enables you to access an element of the array arr
+   andi  $t3, $t2, 1
+   bne   $t3, $zero, change
+   addi  $t0, $t0, 1
+   j     check
+
+change:
+   li    $t1, 0
+
+exit:
+   # the following lines end the program
+   li    $v0, 10
+   syscall
