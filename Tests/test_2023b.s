d6fcf959f3f25c92448c4eb190a82ef943f706b5
Maurice Fallon <mauricefallon@gmail.com>
2024-12-17 20:03:03 +0000

    Your Message

--- Tests/test_2023b.s
+++ Tests/test_2023b.s
@@ -1,31 +1,27 @@
 #2023b
 # the data to be used by the program
 .data
-arr: .word
-2, 4, 1, 8, 10
-len: .word
-5
+arr: .word 2, 4, 1, 8, 10
+len: .word 5
 # the main program
 .text
 main:
-li
-$t0, 0
-li
-$t1, 0
-lw$t7, len
+li $t0, 0  # new version
+li $t1, 0
+lw $t7, len
+
 loop:
-bge$t0, $t7, exit
-sll
-$t6, $t0, 2
-lw
-$t2, arr($t6)
+bge $t0, $t7, exit
+sll $t6, $t0, 2
+lw $t2, arr($t6)
 # arr(...) this is how you access an element of the array arr
-add
-andi$t1, $t1, $t2
-$t3, $t2, 1
-addi
-j$t0, $t0, 1
-loop
+
+add $t1, $t1, $t2
+andi $t3, $t2, 1
+addi $t0, $t0, 1
+j loop
 exit:
 # end of program
 li
+$v0, 10
+syscall
